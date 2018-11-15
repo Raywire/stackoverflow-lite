949858e3a79f0a630164dd050cc575aeddae649e
Ryan Wire <ryanwire@outlook.com>
2018-11-15 18:08:29 +0300

    Signup Functionality

--- routes.py
+++ routes.py
@@ -1,5 +1,5 @@
-from flask import Flask, render_template
-from models import db
+from flask import Flask, render_template, request
+from models import db, User
 from forms import SignupForm
 
 app = Flask(__name__)
@@ -20,7 +20,17 @@ def about():
 @app.route("/signup", methods=['GET', 'POST'])
 def signup():
     form = SignupForm()
-    return render_template("signup.html", form=form)
+
+    if request.method == 'POST':
+        if form.validate() == False:
+            return render_template('signup.html', form=form)
+        else:
+            newuser = User(form.first_name.data, form.last_name.data, form.email.data, form.password.data)
+            db.session.add(newuser)
+            db.session.commit()            
+            return "success"
+    elif request.method == 'GET':
+        return render_template("signup.html", form=form)
 
 if __name__ == "__main__":
     app.run(debug=True)
