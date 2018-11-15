a8fc4b8275cc6220fd0aa21364cd0d707dfde5e8
Ryan Wire <ryanwire@outlook.com>
2018-11-16 00:16:11 +0300

    Aunthentication

--- models.py
+++ models.py
@@ -1,6 +1,8 @@
 from flask_sqlalchemy import SQLAlchemy
 from werkzeug import generate_password_hash, check_password_hash
 
+import datetime
+
 db = SQLAlchemy()
 
 class User(db.Model):
@@ -22,3 +24,28 @@ class User(db.Model):
 
   def check_password(self, password):
     return check_password_hash(self.pwdhash, password)
+
+class Question(db.Model):
+    __tablename__ = 'questions'
+    qid = db.Column(db.Integer, primary_key = True)
+    title = db.Column(db.String(80), nullable = False)
+    body = db.Column(db.Text, nullable = False)
+    date_posted = db.Column(db.DateTime, nullable = False, default=datetime.datetime.utcnow())
+    last_updated = db.Column(db.DateTime, nullable = False, default=datetime.datetime.utcnow())
+    #starter = db.Column(db.Integer,nullable=False, db.ForeignKey('uid.id'))#ForeignKey to reference uid in users table
+    #user = db.relationship('User', backref=db.backref('questions', lazy=True))
+    starter = db.Column(db.Integer)
+    views = db.Column(db.Integer, default=0)
+    #starter = db.Column(db.Integer, db.ForeignKey('user.uid'))
+    #user = db.relationship("User", backref = db.backref("user", uselist=False))
+    def __init__(self, title, body, date_posted, last_updated, starter, views):
+        self.title = title.title()
+        self.body = body.title()
+        self.date_posted = date_posted
+        self.last_updated = last_updated
+        self.starter = starter
+        self.views = views
+'''
+class Answer(db.Model):
+    __tablename__ = 'answers'
+'''
