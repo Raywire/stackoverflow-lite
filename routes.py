1071b7169caf9b9193a6e35a46ab7fba11d2bc48
Ryan Wire <ryanwire@outlook.com>
2018-11-16 12:23:27 +0300

    Add question

--- routes.py
+++ routes.py
@@ -2,6 +2,8 @@ from flask import Flask, render_template, request, session, redirect, url_for
 from models import db, User, Question
 from forms import SignupForm, LoginForm, QuestionForm
 
+import datetime
+
 app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:@Hortec9016@localhost/stackoverflowlite'
@@ -63,19 +65,25 @@ def login():
 def logout():
   session.pop('email', None)
   return redirect(url_for('index'))
-'''
-@app.route("/home")
-def home():
-    if 'email' not in session:
-        return redirect(url_for('login'))
-    return render_template("home.html")
-'''
-@app.route("/new_question")
+
+@app.route("/new_question", methods=["GET", "POST"])
 def new_question():
     if 'email' not in session:
         return redirect(url_for('login'))
     form = QuestionForm()
-    return render_template("new_question.html", form=form)
+
+    if request.method == 'POST':
+        if form.validate() == False:
+            return render_template('new_question.html', form=form)
+        else:
+            newquestion = Question(form.subject.data, form.message.data, date_posted = datetime.datetime.utcnow(), last_updated = datetime.datetime.utcnow(), starter = 1 , views = 0)
+            db.session.add(newquestion)
+            db.session.commit()
+
+            return redirect(url_for('index'))
+    elif request.method == 'GET':
+        return render_template("new_question.html", form=form)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
