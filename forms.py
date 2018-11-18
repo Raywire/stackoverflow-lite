33c660f5e3d90befa288b18cf420f4425b3c5d7d
Ryan Wire <ryanwire@outlook.com>
2018-11-18 19:54:09 +0300

    User Account Information Update Page

--- forms.py
+++ forms.py
@@ -3,17 +3,17 @@ from wtforms import StringField, PasswordField, SubmitField, TextAreaField
 from wtforms.validators import DataRequired, Email, Length, EqualTo
 
 class SignupForm(FlaskForm):
-  first_name = StringField('First name', validators=[DataRequired("Please enter your first name.")])
-  last_name = StringField('Last name', validators=[DataRequired("Please enter your last name.")])
-  email = StringField('Email', validators=[DataRequired("Please enter your email address."), Email("Please enter a valid email address.")])
-  password = PasswordField('Password', validators=[DataRequired("Please enter a password."), Length(min=6, message="Passwords must be 6 characters or more.")])
-  confirm_password = PasswordField('Confirm Password', validators=[DataRequired("Passwords must match"), EqualTo('password')])
-  submit = SubmitField('Sign up')
+    first_name = StringField('First name', validators=[DataRequired("Please enter your first name.")])
+    last_name = StringField('Last name', validators=[DataRequired("Please enter your last name.")])
+    email = StringField('Email', validators=[DataRequired("Please enter your email address."), Email("Please enter a valid email address.")])
+    password = PasswordField('Password', validators=[DataRequired("Please enter a password."), Length(min=6, message="Passwords must be 6 characters or more.")])
+    confirm_password = PasswordField('Confirm Password', validators=[DataRequired("Passwords must match"), EqualTo('password')])
+    submit = SubmitField('Sign up')
 
 class LoginForm(FlaskForm):
-  email = StringField('Email', validators=[DataRequired("Please enter your email address."), Email("Please enter a valid email address.")])
-  password = PasswordField('Password', validators=[DataRequired("Please enter a password."), Length(min=1, message="Enter a valid password")])
-  submit = SubmitField("Sign in")
+    email = StringField('Email', validators=[DataRequired("Please enter your email address."), Email("Please enter a valid email address.")])
+    password = PasswordField('Password', validators=[DataRequired("Please enter a password."), Length(min=1, message="Enter a valid password")])
+    submit = SubmitField("Sign in")
 
 class QuestionForm(FlaskForm):
     subject = StringField('Title', validators=[DataRequired("Please enter a title.")])
@@ -29,3 +29,8 @@ class ResetPasswordForm(FlaskForm):
     password = PasswordField('New Password', validators=[DataRequired("Please enter a password."), Length(min=6, message="Passwords must be 6 characters or more.")])
     confirm_password = PasswordField('Confirm New Password', validators=[DataRequired("Passwords must match"), EqualTo('password')])
     submit = SubmitField("Reset Password")
+
+class UpdateAccountForm(FlaskForm):
+    first_name = StringField('First name', validators=[DataRequired("Please enter your first name.")])
+    last_name = StringField('Last name', validators=[DataRequired("Please enter your last name.")])
+    submit = SubmitField('Update')
